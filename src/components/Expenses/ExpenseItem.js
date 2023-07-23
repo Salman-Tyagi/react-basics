05f1b15ea9c1a68dc1090cbe6e0d05ea8a23fc23
Salman Tyagi <salmantyagi15@gmail.com>
2023-07-23 20:14:11 +0530

    implemented click event in react

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,6 +5,8 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = props => {
+  const clickHandler = () => console.log('Clicked!!!');
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -13,6 +15,7 @@ const ExpenseItem = props => {
         <h2 className="expense-item--title">{props.title}</h2>
         <div className="expense-item--price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
